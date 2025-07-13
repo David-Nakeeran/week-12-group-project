030d16acce1b1703cf1ebabc1552cfd0baf7206d
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-07-13 13:50:41 +0100

    Feat: updated styling

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,27 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
+import fetchFromAPI from "@/lib/rawgApi";
 import heroImg from "@/../public/images/heroImg.jpg";
 
 export default async function LandingPage() {
-  const getGames = async () => {
-    const apiKey = process.env.API_KEY;
-    const url = `https://api.rawg.io/api/games?key=${apiKey}&metacritic=90,100&page_size=6`;
-
-    try {
-      const response = await fetch(url);
-      if (!response) {
-        throw new Error("Failed to fetch data");
-      }
-      const data = await response.json();
-      return data.results;
-    } catch (error) {
-      let message = "Unknown Error";
-      if (error instanceof Error) message = error.message;
-      console.error(message);
-    }
-  };
-
-  const games = (await getGames()) || [];
+  const games = (await fetchFromAPI("metacritic=90,100&page_size=6")) || [];
   console.log(games);
 
   const gameElements = games.map((game) => {
@@ -36,7 +19,7 @@ export default async function LandingPage() {
           style={{ width: "80%", height: "auto" }}
         />
         <p>{game.name}</p>
-        <span>Metacritic: {game.metacritic}</span>
+        <p>Metacritic: {game.metacritic}</p>
         <p>
           {game.genres
             .map((genre) => {
@@ -56,29 +39,33 @@ export default async function LandingPage() {
   });
 
   return (
-    <main className="w-full grid place-items-center">
-      <Image
-        src={heroImg}
-        alt="a close up of keyboard and video game controller"
-        width={348}
-        height={348}
-      />
-      <div className="w-full flex flex-col gap-[1.75rem] mb-[2.5rem]">
-        <h1 className="text-4xl mb-7 font-semibold text-center">
-          Welcome to GameVault
-        </h1>
-        <h3>
-          Your personal vault for tracking, rating, and conquering your video
-          game backlog.
-        </h3>
-        <p>
-          Tired of forgetting what games you&apos;ve finished, started, or never
-          even touched? GameVault helps you organize your entire game collection
-          — from "must plays" to hidden gems — with a clean, retro-inspired
-          interface. Log what you're playing, track your progress, and relive
-          the games you've conquered.
-        </p>
-        <Link href={"/sign-up"}>Join</Link>
+    <main className="w-full grid place-items-center pl-[1.375rem] pr-[1.375rem]">
+      <div className="w-full flex flex-col items-center sm:flex-row">
+        <Image
+          src={heroImg}
+          alt="a close up of keyboard and video game controller"
+          width="0"
+          height="0"
+          sizes="100vw"
+          className="w-full sm:w-3/4 md:w-2/3 lg:w-1/2 h-auto"
+        />
+        <div className="w-full flex flex-col gap-[1.75rem] mb-[2.5rem]">
+          <h1 className="text-4xl mb-7 font-semibold text-center">
+            Welcome to GameVault
+          </h1>
+          <p>
+            Your personal vault for tracking, rating, and conquering your video
+            game backlog.
+          </p>
+          <p>
+            Tired of forgetting what games you&apos;ve finished, started, or
+            never even touched? GameVault helps you organize your entire game
+            collection — from "must plays" to hidden gems — with a clean,
+            retro-inspired interface. Log what you're playing, track your
+            progress, and relive the games you've conquered.
+          </p>
+          <Link href={"/sign-up"}>Join</Link>
+        </div>
       </div>
       <div className="w-full grid grid-cols-[repeat(auto-fit,_minmax(250px,1fr))]">
         {gameElements}
