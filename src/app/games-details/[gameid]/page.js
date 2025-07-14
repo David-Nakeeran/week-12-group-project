5dcd09e9fdb8fa0c8d8137f66b26f3e936704955
Ryan Nugent <ryan.nugent247@gmail.com>
2025-07-14 11:56:53 +0100

    feat: very basic game details page, displays the title and a few bits of infomation. Also added a reviews component thats ready to be coded and used for the page

---
+++ src/app/games-details/[gameid]/page.js
@@ -0,0 +1,48 @@
+import Reviews from "@/components/Reviews";
+import fetchFromAPI from "@/lib/rawgApi";
+import Image from "next/image";
+
+export default async function GameDetailsPage({ params }) {
+  const gameId = params.gameid;
+  const apiKey = process.env.API_KEY;
+  const baseUrl = process.env.API_BASE_URL;
+
+  // For testing, Battlefield hardline Id: 3400 , GTA 5 Id: 3498
+
+  const query = await fetch(`${baseUrl}/${gameId}?key=${apiKey}`);
+
+  const game = await query.json();
+  console.log(game);
+
+  return (
+    <>
+      <h1>{game.name}</h1>
+      <Image
+        src={game.background_image}
+        alt="placeholder"
+        width={400}
+        height={400}
+      />
+      {/* <Image src="#" alt="placeholder" />
+      <Image src="#" alt="placeholder" />
+      <Image src="#" alt="placeholder" /> */}
+      <p>{game.description_raw}</p>
+      <div>
+        <div>
+          <p>Release date:</p>
+          <p>{game.released}</p>
+        </div>
+        <div>
+          <p>Metacritic:</p>
+          <p>{game.metacritic}</p>
+        </div>
+        <div>
+          <p>Platforms:</p>
+          <p>{game.platforms.map((p) => p.platform.name).join(", ")}</p>
+        </div>
+      </div>
+      <h2>Reviews</h2>
+      <Reviews />
+    </>
+  );
+}
---
+++ src/components/Reviews.jsx
@@ -0,0 +1,7 @@
+export default function Reviews() {
+  return (
+    <>
+      <p>A bunch of reviews</p>
+    </>
+  );
+}
