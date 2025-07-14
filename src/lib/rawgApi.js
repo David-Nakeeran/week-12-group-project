2beba9986eba49fa0296e5807accf3e3fa44d4b7
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-07-14 19:03:42 +0100

    Chore: updated fetches

--- src/lib/rawgApi.js
+++ src/lib/rawgApi.js
@@ -2,7 +2,41 @@ const apiKey = process.env.API_KEY;
 const baseUrl = process.env.API_BASE_URL;
 
 export default async function fetchFromAPI(query = "") {
-  const url = `${baseUrl}?key=${apiKey}${query ? `&${query}` : ""}`;
+  const url = `${baseUrl}/games?key=${apiKey}${query ? `&${query}` : ""}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response) {
+      throw new Error("Failed to fetch data");
+    }
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    let message = "Unknown Error";
+    if (error instanceof Error) message = error.message;
+    console.error(message);
+  }
+}
+
+export async function fetchGenresFromAPI() {
+  const url = `${baseUrl}/genres?key=${apiKey}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response) {
+      throw new Error("Failed to fetch data");
+    }
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    let message = "Unknown Error";
+    if (error instanceof Error) message = error.message;
+    console.error(message);
+  }
+}
+
+export async function fetchPlatformsFromAPI() {
+  const url = `${baseUrl}/platforms/lists/parents?key=${apiKey}`;
 
   try {
     const response = await fetch(url);
