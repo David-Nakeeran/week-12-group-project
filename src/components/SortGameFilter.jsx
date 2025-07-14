df17790846bb9bf2e57d0aed665d4926190c9301
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-07-14 18:02:23 +0100

    Feat: sort functionality

---
+++ src/components/SortGameFilter.jsx
@@ -0,0 +1,59 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function SortGameFilter({ genres, platforms }) {
+  const [genre, setGenre] = useState("");
+  const [platform, setPlatform] = useState("");
+  const router = useRouter();
+
+  const genreElements = genres.map((element) => {
+    return (
+      <option key={element.id} value={element.slug}>
+        {element.name}
+      </option>
+    );
+  });
+
+  const platformElements = platforms.map((element) => {
+    return (
+      <option key={element.id} value={element.id}>
+        {element.name}
+      </option>
+    );
+  });
+
+  function updateParams(selectedGenre, selectedPlatform) {
+    const params = new URLSearchParams();
+
+    if (selectedGenre) params.set("genre", selectedGenre);
+    if (selectedPlatform) params.set("platform", selectedPlatform);
+
+    router.push(`/games-catalogue?${params.toString()}`);
+  }
+
+  const handleGenreChange = (e) => {
+    const selectedGenre = e.target.value;
+    setGenre(selectedGenre);
+    updateParams(selectedGenre, platform);
+  };
+
+  const handlePlatformChange = (e) => {
+    const selectedPlatform = e.target.value;
+    setPlatform(selectedPlatform);
+    updateParams(genre, selectedPlatform);
+  };
+
+  return (
+    <div>
+      <select onChange={handleGenreChange} defaultValue={"Genre"}>
+        <option disabled="disabled">Genre</option>
+        {genreElements}
+      </select>
+      <select onChange={handlePlatformChange} defaultValue={"Platform"}>
+        <option disabled="disabled">Platform</option>
+        {platformElements}
+      </select>
+    </div>
+  );
+}
