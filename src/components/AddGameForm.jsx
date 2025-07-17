ba4300f797d9d5c72369cd2acf969acdd5173e43
Ryan Nugent <ryan.nugent247@gmail.com>
2025-07-17 14:38:19 +0100

    added cursor pointers to buttons that were missing them

--- src/components/AddGameForm.jsx
+++ src/components/AddGameForm.jsx
@@ -34,25 +34,25 @@ export default function AddGameForm({
           name="status"
           required
           defaultValue={gameStatus || "W"}
-          className=" w-full  rounded-lg p-2 mt-2 border border-neutral-400"
+          className=" w-full  rounded-lg p-2 mt-2 border border-neutral-400 cursor-pointer"
         >
-          <option value="W" className="text-neutral-900">
+          <option value="W" className="text-neutral-900 cursor-pointer">
             Wishlist
           </option>
-          <option value="O" className="text-neutral-900">
+          <option value="O" className="text-neutral-900 cursor-pointer">
             Ongoing
           </option>
-          <option value="C" className="text-neutral-900">
+          <option value="C" className="text-neutral-900 cursor-pointer">
             Completed
           </option>
-          <option value="D" className="text-neutral-900">
+          <option value="D" className="text-neutral-900 cursor-pointer">
             Dropped
           </option>
         </select>
 
         <button
           type="submit"
-          className="w-full bg-[#2A2A2A] text-[#FFFFFF] font-bold pt-[0.75rem] pb-[0.75rem] pr-[1rem] pl-[1rem] text-center rounded-[0.625em] mb-[0.75rem] mt-[0.75rem] hover:bg-[#3a3a3a] hover:scale-105 transform transition-colors duration-200"
+          className="w-full bg-[#2A2A2A] text-[#FFFFFF] font-bold pt-[0.75rem] pb-[0.75rem] pr-[1rem] pl-[1rem] text-center rounded-[0.625em] mb-[0.75rem] mt-[0.75rem] hover:bg-[#3a3a3a] hover:scale-105 transform transition-colors duration-200 cursor-pointer"
         >
           {hasGame ? "Update Game Status" : "Submit game status"}
         </button>
